59a99cf6745a719e23441d1bf36325a936bf6ff3
kaka0902669477 <64117392+kaka0902669477@users.noreply.github.com>
2020-08-26 22:35:04 +0700

    12

--- OOP_PROJECT/Banking_Record_System/Manager.h
+++ OOP_PROJECT/Banking_Record_System/Manager.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef _MANAGER_H_
+#define _MANAGER_H_
+
+#include "Header.h"
+
+class ManagerMenu
+{
+private:
+	int _mode;
+	Account _account;
+	vector<Request> _requestList;
+public:
+	ManagerMenu(Account acc);
+	void showMenu();
+	void viewRequest();
+	void editRequest();
+};
+
+#endif
